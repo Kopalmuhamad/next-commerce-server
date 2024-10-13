139dceac2f4b8b58744c4e16d9bd955e01afe67e
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-10-14 00:05:46 +0700

    add lean() for speeding function

--- app.js
+++ app.js
@@ -39,11 +39,17 @@ app.listen(port, () => {
 });
 
 //? Connect to mongodb
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully.");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // Exit the process if connection fails
+  }
+};
+
+connectDB(); // Call connection when server starts
