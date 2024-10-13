f51bcb8f191611b3d6ae0633efaf5e2219595e85
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-10-14 00:11:32 +0700

    error:FUNCTION_INVOCATION_FAILED

--- app.js
+++ app.js
@@ -39,14 +39,9 @@ app.listen(port, () => {
 });
 
 //? Connect to mongodb
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully.");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error);
-    process.exit(1); // Keluar jika koneksi gagal
-  }
-};
-
-connectDB(); // Call connection when server starts
+try {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("MongoDB connected successfully.");
+} catch (error) {
+  console.error("MongoDB connection failed:", error);
+}
